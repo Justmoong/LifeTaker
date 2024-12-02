b12df93e8bc0d52b02210239af3674ca5fe34461
Justmoong <andudyun0504@gmail.com>
2024-12-02 18:14:43 +0900

    Add Data Manager

--- Chrono/Model/UserData.swift
+++ Chrono/Model/UserData.swift
@@ -5,19 +5,58 @@
 //  Created by 윤무영 on 11/8/24.
 //
 
-//import Foundation
-//import SwiftData
-//
-//struct UserProfile : Codable {
-//    var userName: String
-//    var userAge: Int
-//    var expectedLifespan: Int
-//    
-//    init(userName: String, userAge: Int, expectedLifespan: Int) {
-//        self.userName = userName
-//        self.userAge = userAge
-//        self.expectedLifespan = expectedLifespan
-//    }
-//}
-//
-//
+import Foundation
+
+struct UserData: Codable {
+    var userName: String
+    var userAge: Int
+    var userBirthDay: Date
+    var expectedLifespan: Int
+    var userSex: String
+    
+    // 기본값 생성
+    static var defaultData: UserData {
+        UserData(
+            userName: "",
+            userAge: 0,
+            userBirthDay: Date(),
+            expectedLifespan: 100,
+            userSex: "Male"
+        )
+    }
+}
+
+// 사용자 데이터 관리 클래스
+class UserDataManager: ObservableObject {
+    @Published var userData: UserData = UserData.defaultData {
+        didSet {
+            saveUserData()
+        }
+    }
+    
+    private let userDefaultsKey = "UserData"
+    private let userDefaults = UserDefaults(suiteName: "group.com.moonglab.chrono")!
+    
+    init() {
+        loadUserData()
+    }
+    
+    // 데이터 저장
+    private func saveUserData() {
+        guard let encodedData = try? JSONEncoder().encode(userData) else {
+            print("Failed to encode user data.")
+            return
+        }
+        userDefaults.set(encodedData, forKey: userDefaultsKey)
+    }
+    
+    // 데이터 로드
+    private func loadUserData() {
+        guard let savedData = userDefaults.data(forKey: userDefaultsKey),
+              let decodedData = try? JSONDecoder().decode(UserData.self, from: savedData) else {
+            print("No user data found, loading default data.")
+            return
+        }
+        userData = decodedData
+    }
+}
--- Chrono/View/HomeView.swift
+++ Chrono/View/HomeView.swift
@@ -10,11 +10,8 @@ import SwiftData
 
 struct HomeView: View {
     
-    @AppStorage("userName", store: UserDefaults(suiteName: "group.com.moonglab.chrono")) public var userName: String = ""
-    @AppStorage("userAge", store: UserDefaults(suiteName: "group.com.moonglab.chrono")) var userAge: Int = 0
-    @AppStorage("userBirthDay", store: UserDefaults(suiteName: "group.com.moonglab.chrono")) var userBirthDay: Date = Date()
-    @AppStorage("expectedLifespan", store: UserDefaults(suiteName: "group.com.moonglab.chrono")) var expectedLifespan: Int = 100
-    @AppStorage("userSex", store: UserDefaults(suiteName: "group.com.moonglab.chrono")) var userSex: String = "Male"
+    @StateObject private var userData = UserDataManager()
+    @StateObject private var eventData = EventDataManager()
     //edit UserPrifle mode on sheet
     @State var isPresented: Bool = false
     //Events model
@@ -31,18 +28,20 @@ struct HomeView: View {
     var body: some View {
         List {
             Section(header: EmptyView()) {
-                UserProfileView(showingName: $userName,
-                                userAge: $userAge,
-                                userBirthDay: $userBirthDay,
-                                userExpectedLifespan: $expectedLifespan
+                UserProfileView(
+                    showingName: $userData.userData.userName,
+                    userAge: $userData.userData.userAge,
+                    userBirthDay: $userData.userData.userBirthDay,
+                    userExpectedLifespan: $userData.userData.expectedLifespan
                 )
                 .sheet(isPresented: $isPresented) {
                     InputUserInfoView(
-                        inputedName: $userName,
-                        userBirthday: $userBirthDay,
-                        userAge: $userAge,
-                        userSex: $userSex,
-                        expectedLifespan: $expectedLifespan)
+                        inputedName: $userData.userData.userName,
+                        userBirthday: $userData.userData.userBirthDay,
+                        userAge: $userData.userData.userAge,
+                        userSex: $userData.userData.userSex,
+                        expectedLifespan: $userData.userData.expectedLifespan
+                    )
                 }
                 .onTapGesture {
                     isPresented.toggle()
@@ -52,19 +51,12 @@ struct HomeView: View {
                 EventsListView(event: $event)
                     .onAppear() //뷰가 표시될 때 변수들을 print로 출력
                 {
-                    
-                    
                     print("EventsArray.0.name set: \(event[0].name)")
                     print("EventsArray.0.DDay set: \(event[0].DDay)")
                     print("EventsArray.0.gaugeValue set: \(event[0].gaugeValue)")
                     print("EventsArray.0.min set: \(event[0].min)")
                     print("EventsArray.0.max set: \(event[0].max)\n")
-                    
-//                    print("EventsArray.1.name set: \(event[1].name)")
-//                    print("EventsArray.1.DDay set: \(event[1].DDay)")
-//                    print("EventsArray.1.gaugeValue set: \(event[1].gaugeValue)")
-//                    print("EventsArray.1.min set: \(event[1].min)")
-//                    print("EventsArray.1.max set: \(event[1].max)\n")
+
                 }
             }
         }
