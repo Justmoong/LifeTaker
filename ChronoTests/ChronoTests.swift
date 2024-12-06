f166a99ef53262c68aeac546ad4d83ee3d441566
Justmoong <andudyun0504@gmail.com>
2024-12-06 15:40:56 +0900

    Set Unit Test

---
+++ ChronoTests/ChronoTests.swift
@@ -0,0 +1,44 @@
+//
+//  ChronoTests.swift
+//  ChronoTests
+//
+//  Created by 윤무영 on 12/5/24.
+//
+
+import Foundation
+import XCTest
+
+final class ChronoTests: XCTestCase {
+
+    func testHelloWorld_ConstString_ConstString() {
+        //PreCondition
+        let message = "Hello, Swift!"
+        //Action
+        func chString (what: String) -> String {
+           let content = "Hello, World!"
+            return content
+        }
+        print(#function,chString(what: message))
+        //PostCondition && Assert
+        XCTAssertEqual(chString(what: message), "Hello, World!")
+    }
+    
+    func testSetBirthDay_DateComponent_String() {
+        //PreCondition
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        dateFormatter.timeZone = TimeZone(identifier: "Asia/Seoul")
+        
+        var dateComponents = DateComponents()
+        dateComponents.year = 2002
+        dateComponents.month = 5
+        dateComponents.day = 4
+        //Action
+        let resultDate = Calendar.current.date(from: dateComponents)
+
+        print(#function, dateFormatter.string(from: resultDate!))
+        //PostCondition && Assert
+        XCTAssert(resultDate != nil)
+    }
+
+}
