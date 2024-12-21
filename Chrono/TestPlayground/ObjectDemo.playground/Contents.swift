f51b87419cf55869ae08c8d60d44df394848779a
Justmoong <andudyun0504@gmail.com>
2024-12-22 02:53:13 +0900

    Add New Playground

---
+++ Chrono/TestPlayground/ObjectDemo.playground/Contents.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+class ChrisrtmasProperties {
+    static func remainingChristmasDays() -> Int {
+        return 0
+    }
+    
+    static func daysPassedInYear() -> Int {
+        return 0
+    }
+}
